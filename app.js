ffdeb68d854d314c115ea757a27912fcc9249bc6
Matt <hello@mattnull.com>
2013-02-18 15:43:18 -0700

    removed us unnecessary events

--- app.js
+++ app.js
@@ -4,14 +4,13 @@ var webRTC = require('webrtc.io').listen(server);
 
 server.listen(8000);
 
+// Routes
 app.get('/', function(req, res) {
   res.sendfile(__dirname + '/index.html');
 });
-
 app.get('/main.css', function(req, res) {
   res.sendfile(__dirname + '/public/main.css');
 });
-
 app.get('/webrtc.io.js', function(req, res) {
   res.sendfile(__dirname + '/public/webrtc.io.js');
 });
@@ -23,18 +22,5 @@ app.get('/prettify.min.js', function(req, res) {
 });
 app.get('/main.js', function(req, res) {
   res.sendfile(__dirname + '/public/main.js');
-});
-
-//webRTC socet events
-webRTC.rtc.on('connect', function(rtc) {
-  //Client connected
-});
-
-webRTC.rtc.on('send answer', function(rtc) {
-  //answer sent
-});
-
-webRTC.rtc.on('disconnect', function(rtc) {
-  //Client disconnect 
 });
 
